17fd7ff16b25c0680d6b4537308c0de8bf581eb1
talididai <tali.didai@hotmail.com>
2019-10-15 11:01:28 -0300

    ajuste

--- permission.sh
+++ permission.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -n -e '\033[31m Digite o caminho da pasta: \033[m'
+echo -n -e '\033[31m Digite o caminho para a pasta: \033[m'
 
 read FOLDER
 ls -l $FOLDER | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/) *2^(8-i));if(k)printf("%0o ",k);print}'
